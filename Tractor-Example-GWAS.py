a232e1809c8a4dec15dbb7ee1fa26f34f64b1b90
Elizabeth Atkinson <egatkinson@mgh.harvard.edu>
2020-06-01 17:13:23 -0400

    Update Tractor-Example-GWAS.py

--- Tractor-Example-GWAS.py
+++ Tractor-Example-GWAS.py
@@ -2,17 +2,10 @@
 # coding: utf-8
 
 # # Initialize Hail
-
-# In[1]:
-
-
 import argparse
 import hail as hl
 import numpy as np
 hl.init()
-
-
-# In[2]:
 
 
 #load in plotting features
@@ -21,134 +14,86 @@ from pprint import pprint
 hl.plot.output_notebook()
 
 
-# # Load in the filtered and phenotype annotated genotype data for the individuals
-# ### Note: the example file is in Matrix Table format, the native Hail format. VCF formats may also be imported with 'hl.import_vcf'. See documentation for more details: https://hail.is/
-
-# In[ ]:
-
-
-#key columns by sample and rows by rsID for easier merge with dosage data
-mt = hl.read_matrix_table('gs://ukb-diverse-pops/AdmixedAfrEur/Results/UKBB_AfEur_QCed_lipids2.mt').key_rows_by('locus')
-
-
 # # Load in the dosage files from Tractor
 # ### Note: this will be the most time intensive step. Hail team is actively optimizing pieces of this infrastructure.
+## user should modify the paths in the import steps to match the location (here shown for files on google cloud) of their datasets
 
-# In[ ]:
-
-
+#start loading in the ancestry 0 minor allele dosages
 row_fields={'CHROM': hl.tstr, 'POS': hl.tint, 'ID': hl.tstr, 'REF': hl.tstr, 'ALT': hl.tstr} 
-anc0dos = hl.import_matrix_table('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids.autosomes.anc0.dosage_v1.txt.gz', 
-                                 force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
+anc0dos = hl.import_matrix_table('gs://.../Dataset.anc0.dosage.txt.gz', force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
 anc0dos = anc0dos.key_rows_by().drop('row_id')
 anc0dos = anc0dos.key_rows_by(locus=hl.locus(anc0dos.CHROM, anc0dos.POS))     
 
 
-# In[ ]:
-
-
+#also load ancestry 1 allele dosages
 row_fields={'CHROM': hl.tstr, 'POS': hl.tint, 'ID': hl.tstr, 'REF': hl.tstr, 'ALT': hl.tstr} 
-anc1dos = hl.import_matrix_table('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids.autosomes.anc1.dosage_v1.txt.gz', 
-                                 force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
+anc1dos = hl.import_matrix_table('gs://.../Dataset.anc1.dosage.txt.gz', force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
 anc1dos = anc1dos.key_rows_by().drop('row_id')
 anc1dos = anc1dos.key_rows_by(locus=hl.locus(anc1dos.CHROM, anc1dos.POS))     
 
-
-# In[ ]:
-
-
 #Optional - save these temporary files to relieve  memory burden
-anc1dos = anc1dos.checkpoint('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids.autosomes.anc1.dosage_v1.mt')
-anc0dos = anc0dos.checkpoint('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids.autosomes.anc0.dosage_v1.mt')
+anc0dos = anc0dos.checkpoint('gs://.../Dataset.anc0.dosage.mt')
+anc1dos = anc1dos.checkpoint('gs://.../Dataset.anc1.dosage.mt')
 
 
-# In[ ]:
-
-
-#read in haplotype counts for anc0, here African, the index ancestry
+#read in haplotype counts for the index ancestry, here anc0
 row_fields={'CHROM': hl.tstr, 'POS': hl.tint, 'ID': hl.tstr} 
-hapcounts0 = hl.import_matrix_table('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids.auto.anc0.hapcount.txt.gz', 
-                                 force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
+hapcounts0 = hl.import_matrix_table('gs://.../Dataset.anc0.hapcount.txt.gz', force_bgz=True, row_fields=row_fields, row_key=[], min_partitions=32) 
 hapcounts0 = hapcounts0.key_rows_by().drop('row_id')
 hapcounts0 = hapcounts0.key_rows_by(locus=hl.locus(hapcounts0.CHROM, hapcounts0.POS))   
 
+##if more than 2 way admixed, load in additional ancestry files with same sytax
 
-# In[ ]:
+#join the ancestry and haplotype dosage files together into a Hail matrix table
+mt = anc0dos.annotate_entries(anc1dos = anc1dos[anc0dos.locus, anc0dos.col_id], hapcounts0 = hapcounts0[mt.locus, mt.col_id])
 
+#Here's an example showing the implementation if cohort data is 3 way admixed
+#mt = anc0dos.annotate_entries(anc1dos = anc1dos[anc0dos.locus, anc0dos.col_id], anc2dos = anc2dos[anc0dos.locus, anc0dos.col_id], hapcounts0 = hapcounts0[mt.locus, mt.col_id], hapcounts1 = hapcounts1[mt.locus, mt.col_id]))
 
-#join the dosage files to the genotype data. 
-#Specifically, annotating the samples with their info for how many copies of the minor allele per ancestry were seen and index haplotype counts
-mt = mt.annotate_entries(anc0dos = anc0dos[mt.locus, mt.s], anc1dos = anc1dos[mt.locus, mt.s], hapcounts0 = hapcounts0[mt.locus, mt.s])
+## load in the phenotype phenotype and covariate file. The IDs should be consistent with those in the header of the Tractor dosage file
+phenos = hl.import_table('gs://ukb-diverse-pops/AdmixedAfrEur/NHLP/nhlp_nov19_pheno_and_covs.txt').key_by('IID')
 
+#annotate the mt with the phenotypes
+mt = mt.annotate_cols(phenos = phenos[mt.col_id])
 
-# In[ ]:
-
-
-#write out the newly annotated matrix table. Will run a lot faster if we load this in again after annotating things in due to Hail processing style.
+#write out the newly annotated matrix table. Will run a lot faster if we load this in again after annotating things in due to Hail's lazy processing style.
 mt.write('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids_dosages.mt')
 
 
-# # Run linear regression across all phenotypes.
-
-# In[3]:
-
+# # Run Tractor ancestry-aware linear regression
 
 #read in the filtered and annotated mt file again to go faster
 mt = hl.read_matrix_table('gs://ukb-diverse-pops/AdmixedAfrEur/DosageFiles/UKBB_AfEur_QCed_lipids_dosages.mt')
 
-
-# In[4]:
-
-
-#Run GWAS on a single phenotype, here running on TC, Total Cholesterol, using the Tractor haplotype counts and dosages
-#along with the covariates of ages, sex, blood dilution level, and global ancestry fractions obtained from previous ADMIXTURE runs
+#Run GWAS on a single phenotype, here running on TC, Total Cholesterol, using the Tractor haplotype counts and dosages as described in the manuscript
+#along with relevant covariates, here we show the covariates being sex, age, blood dilution level, and global ancestry fractions obtained from previous ADMIXTURE runs
+#We are annotating the matrix table with the results of the GWAS. The results are saved as a struct here called TC
 mt = mt.annotate_rows(TC = hl.agg.linreg(mt.TC,
                                                  [1.0, mt.hapcounts0.x, mt.anc0dos.x, mt.anc1dos.x, mt.isMale, mt.age, mt.dilutionFactor, hl.float(mt.admixFrac.AFR)]))
 
 
-# In[45]:
-
-
-#to run phenotyps in batch, make a list of the phenotype names
+#to run phenotyps in batch, first make a list of the phenotype names. Here showing 4 blood lipid phenotypes
 phenonames = ['TG', 'TC', 'HDLC', 'LDLC']
 
-
-# In[5]:
-
-
-#run GWAS in batch for all listed phenotypes, annotating with the pheno name
+#run GWAS in batch for all listed phenotypes, annotating results in mt as separate structs named after the pheno
 mt = mt.annotate_rows(results=hl.struct(**{pheno: hl.agg.linreg(mt[pheno], 
                                                                 [1.0, mt.hapcounts0.x, mt.anc0dos.x, mt.anc1dos.x, mt.isMale, mt.age, mt.dilutionFactor, hl.float(mt.admixFrac.AFR)]) 
                                            for pheno in phenonames}))
 
 
-# In[ ]:
-
-
-#plot up as a manhattan plot, anc 0 and anc 1 seperately
-#in the implementation above, the first p value in the array is for the intercept, the second is ancestry counts at the site, the third is anc0, and the fourth is anc1, followed by the covariates
+#generate manhattan plots with bokeh, anc 0 and anc 1 are plotted seperately
+#in the implementation above, the first (0 indexed) p value in the array is for the intercept, the second is ancestry haplotype counts at the site, the third is anc0, and the fourth is anc1, followed by the covariates
 p = hl.plot.manhattan(mt.results.TC.p_value[2], title='Admixed Afr-Eur UKBB, TC, anc0', collect_all=False, significance_line=5e-08) 
 show(p)
-
-
-# In[ ]:
-
 
 #plot up as a manhattan plot, anc 1
 p = hl.plot.manhattan(mt.results.TC.p_value[3], title='Admixed Afr-Eur UKBB, TC, anc1', collect_all=False, significance_line=5e-08) 
 show(p)
 
 
-# In[ ]:
-
-
 #make a QQ plot for TC anc0
 p = hl.plot.qq(mt.results.TC.p_value[2], title="QQ plot, TC, anc0")
 show(p)
-
-
-# In[ ]:
-
 
 #make a QQ plot for TC anc1
 p = hl.plot.qq(mt.results.TC.p_value[3], title="QQ plot, TC, anc1")
